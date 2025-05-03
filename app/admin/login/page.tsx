a9c7ca76b5503542e96be55ed5f02f2ec26ed279
Magnus Dahlgren <m.dahlgren@me.com>
2025-05-03 10:00:48 +0100

    Refactors from code review

--- app/admin/login/page.tsx
+++ app/admin/login/page.tsx
@@ -6,8 +6,6 @@ import { supabase } from '@/lib/supabase';
 
 export default function LoginPage() {
   const [email, setEmail] = useState('');
-  const [message, setMessage] = useState('');
-  const [error, setError] = useState('');
   const [success, setSuccess] = useState(false);
   const [buttonDisabled, setButtonDisabled] = useState(false);
   const [shake, setShake] = useState(false);
@@ -15,8 +13,6 @@ export default function LoginPage() {
   async function handleMagicLink(e: React.FormEvent) {
     e.preventDefault();
     setButtonDisabled(true);
-    setError('');
-    setMessage('');
 
     const { error } = await supabase.auth.signInWithOtp({
       email,
@@ -25,26 +21,28 @@ export default function LoginPage() {
       },
     });
 
-    if (!error) {
-      setSuccess(true);
-    }
-
-    setTimeout(() => {
-      setButtonDisabled(false);
-      if (!success) {
+    if (error) {
+      setTimeout(() => {
+        setButtonDisabled(false);
         setShake(true);
-        setTimeout(() => setShake(false), 400); // match the animation duration
-      }
-    }, 5000);
+        setTimeout(() => setShake(false), 400);
+      }, 5000);
+    } else {
+      setSuccess(true);
+      setTimeout(() => setButtonDisabled(false), 5000);
+    }
   }
 
-  let buttonText = 'Send magic link';
-  if (success && buttonDisabled) {
-    buttonText = 'Email sent';
-  } else if (buttonDisabled) {
-    buttonText = 'Processing';
-  } else if (success) {
-    buttonText = 'Re-send magic link';
+  function getButtonText() {
+    if (success && buttonDisabled) {
+      return 'Email sent';
+    } else if (buttonDisabled) {
+      return 'Processing';
+    } else if (success) {
+      return 'Re-send magic link';
+    } else {
+      return 'Send magic link';
+    }
   }
 
   return (
@@ -68,10 +66,8 @@ export default function LoginPage() {
           }}
           required
         />
-        {error && <p className="message error">{error}</p>}
-        {message && <p className="message success">{message}</p>}
         <button className="primary-button" disabled={buttonDisabled} type="submit">
-          {buttonText}
+          {getButtonText()}
         </button>
       </form>
     </main>
